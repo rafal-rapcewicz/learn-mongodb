5c3cbde2acb23341444c7536f562ba9abac8dea2
Rafal Rapcewicz <rafal.rapcewicz@gmail.com>
2019-07-23 11:17:38 +0200

    added server validation

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,33 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { Cursor, MongoError } from 'mongodb';
 import path from 'path';
 import cors from 'cors';
+import * as joi from 'joi';
 import * as db from './db';
 
 const collection = 'listingsAndReviews';
 const app: Application = express();
+const schema = joi.object().keys({
+    name: joi.string().required(),
+    notes: joi.string(),
+    property_type: joi.string(),
+    room_type: joi.string(),
+    house_rules: joi.string()
+});
 
 app.use(
     bodyParser.json(),
-    cors()
+    cors(),
+
+    // in tutorial author added message and error to successful responses to use it for notifications..
+    <ErrorRequestHandler>(error: any, request: Request, response: Response, next: NextFunction) => {
+        response.status(error.status).json({
+            error: {
+                message: error.message
+            }
+        });
+    }
 );
 
 app.get('/airbnb', (request: Request, response: Response) => {
@@ -77,26 +94,40 @@ app.get('/:id', (request: Request, response: Response) => {
 });
 
 // update
-app.put('/:id', (request: Request, response: Response) => {
+app.put('/:id', (request: Request, response: Response, next: NextFunction) => {
     const id = request.params.id;
     const properties = request.body;
     const database = db.getDB();
+    const handleError = (message: string, status: number, next: NextFunction) => {
+        const _error = new Error(message);
+        (<any>_error).status = status;
+        next(_error);
+    }
 
     if (database) {
-        database.collection(collection)
-            .findOneAndUpdate(
-                { _id: id /*db.getPrimaryKey(id)*/ },
-                { $set: properties },
-                { returnOriginal: false },
-                (error, result) => {
-                    if (error) {
-                        console.log(error); // notify user instead
-                        return;
-                    }
 
-                    response.json(result);
-                }
-            );
+        // exmple of validation
+        joi.validate(properties, schema, (error: joi.ValidationError, result) => {
+            if (error) {
+                handleError('Invalid input', 500, next);
+                return;
+            }
+
+            database.collection(collection)
+                .findOneAndUpdate(
+                    { _id: id /*db.getPrimaryKey(id)*/ },
+                    { $set: properties },
+                    { returnOriginal: false },
+                    (error, result) => {
+                        if (error) {
+                            handleError('Failed to update', 500, next);
+                            return;
+                        }
+
+                        response.json(result);
+                    }
+                );
+        });
     }
 });
 
